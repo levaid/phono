a7b84ef386cd130169f9adfeec253856e4111e92
kulvirag <kulvirag@gmail.com>
2020-11-12 12:58:39 +0100

    dzs 👳

--- processing/converter.py
+++ processing/converter.py
@@ -70,8 +70,9 @@ class PhonemeConverter():
 
 
     def optimal_process(self, sentence: str):
-        sentence = self.x_letter(sentence)
+        #sentence = self.x_letter(sentence)
         sentence = self.fast_double_letters(sentence)
+        sentence = self.fast_h_transformation(sentence)
         sentence = self.fast_degemination(sentence)
         sentence = self.fast_n_assimilation(sentence)
         sentence = self.fast_palatal_assimilation(sentence)
@@ -136,8 +137,8 @@ class PhonemeConverter():
 
 
     def fast_double_letters(self, sentence: str):
-        double_long = [('ccs','Č'),('ddz','Ď'),('ggy','Ǧ'),('lly','J'),('nny','Ɲ'),('ssz','Ʃ'),('tty','Ť'),('zzs','Ž')]
-        double = [('cs','č'),('dz','ď'),('gy','ǧ'),('ly','j'),('ny','ɲ'),('sz','ʃ'),('ty','ť'),('zs','ž')]
+        double_long = [('ccs','Č'),('ddz','Ď'),('ddzs','Ĵ'),('ggy','Ǧ'),('lly','J'),('nny','Ɲ'),('ssz','Ʃ'),('tty','Ť'),('zzs','Ž')]
+        double = [('cs','č'),('dz','ď'),('dzs','ĵ'),('gy','ǧ'),('ly','j'),('ny','ɲ'),('sz','ʃ'),('ty','ť'),('zs','ž')]
         for old, new in double_long + double:
             sentence = sentence.replace(old, new)
 
@@ -153,12 +154,12 @@ class PhonemeConverter():
     def fast_h_transformation(self, sentence: str):
         sentence = re.sub(r'([aáeéiíoóöőüűuú][|§#~ ]?)h([|§#~ ]?[aáeéiíoóöőüűuú])',r'\g<1>ɦ\g<2>',sentence) #tehén
         sentence = re.sub(r'([aáeéiíoóöőüűuú][|§#~ ]?)H([|§#~ ]?[aáeéiíoóöőüűuú])',r'\g<1>Ḧ\g<2>',sentence) #ahhoz
-        sentence = re.sub(r'([aáeéiíoóöőüűuú][|§#~]?)c?h([|§#~]?[bcdfgjklmnpqrstvxzčďǧɲʃťž])',r'\g<1>ḧ\g<2>',sentence) ##pechből
+        sentence = re.sub(r'([aáeéiíoóöőüűuú][|§#~]?)[c]?h([|§#~]?[bcdfgjklmnpqrstvxzčďǧɲʃťž ]?)',r'\g<1>ḧ\g<2>',sentence) ##pechből
         return(self.fast_long_letters(sentence))
 
     def nasalisation(self, sentence: str):
         sentence = re.sub(r'(n)([|~§#]?[pbfv])',r'm\g<2>',sentence)
-        sentence = re.sub(r'(n)([|~§#]?[ǧť])',r'ɲ\g<2>',sentence)
+        sentence = re.sub(r'(n)([|~§#]?[ǧť])',r'ɲ\g<2>',sentence) 
         sentence = re.sub(r'(n)([|~§#]?ɲ)',r'Ɲ',sentence)
         return(self.fast_double_letters(sentence))
 
@@ -261,7 +262,7 @@ class PhonemeConverter():
     def hiatus_filling(self, sentence: str):
         vowels = 'aáeéoóöőüűuú'
         sentence = re.sub(f'i[|~§]?([{vowels}])',r'ij\g<1>',sentence)
-        sentence = re.sub(f'([{vowels}])i[|~§]?',r'\g<1>ji',sentence)
+        sentence = re.sub(f'([{vowels}])[|~§]?i',r'\g<1>ji',sentence)
         return(sentence)
 
     
@@ -316,20 +317,20 @@ class PhonemeConverter():
         sentence = re.sub(r'([BCDFGHJKLMNPQRSTVWXZČĎǦƝƩŤŽ])([|§~# ]?)([bcdfghjklmnpqrstvwxzčďǧɲʃťž])', lambda m: m.group(1).lower()+m.group(2)+m.group(3),sentence)
 
         # print(re.findall(r'([bcdfghjklmnpqrstvwxzčďǧɲʃťž]([bcdfghjklmnpqrstvwxzčďǧɲʃťž]))[|#§~ ]\2', sentence))
-        sentence = re.sub(r'([bcdfghjklmnpqrstvwxzčďǧɲʃťž]([bcdfghjklmnpqrstvwxzčďǧɲʃťž]))[|#§~ ]\2',r'\g<1>', sentence)
+        sentence = re.sub(r'([bcdfghjklmnpqrstvwxzčďǧɲʃťž]([bcdfghjklmnpqrstvwxzčďǧɲʃťž]))[|#§~ ]?\2',r'\g<1>', sentence)
         # print('after______', sentence)
 
         # print(re.findall(r'([bcdfghjklmnpqrstvwxzčďǧɲʃťž])([BCDFGHJKLMNPQRSTVWXZČĎǦƝƩŤŽ])', sentence))
-        sentence = re.sub(r'([bcdfghjklmnpqrstvwxzčďǧɲʃťž])[|~#§]([BCDFGHJKLMNPQRSTVWXZČĎǦƝƩŤŽ])', lambda m: m.group(1)+m.group(2).lower(), sentence)
+        sentence = re.sub(r'([bcdfghjklmnpqrstvwxzčďǧɲʃťž])[|~#§]?([BCDFGHJKLMNPQRSTVWXZČĎǦƝƩŤŽ])', lambda m: m.group(1)+m.group(2).lower(), sentence)
 
         # print(re.findall(r'([bcdfghjklmnpqrstvwxzčďǧɲʃťž])[|~#§ ](\1[bcdfghkpqstvwxzčďǧʃťž])', sentence))
-        sentence = re.sub(r'([bcdfghjklmnpqrstvwxzčďǧɲʃťž])[|~#§ ](\1[bcdfghkpqstvwxzčďǧʃťž])',r'\g<2>',sentence)
+        sentence = re.sub(r'([bcdfghjklmnpqrstvwxzčďǧɲʃťž])[|~#§ ]?(\1[bcdfghkpqstvwxzčďǧʃťž])',r'\g<2>',sentence)
         
         # print(re.findall(r'([BCDFGHJKLMNPQRSTVWXZČĎǦƝƩŤŽ])([bcdfghkpqstvwxzčďǧʃťž])', sentence))
         sentence = re.sub(r'([BCDFGHJKLMNPQRSTVWXZČĎǦƝƩŤŽ])([bcdfghkpqstvwxzčďǧʃťž])', lambda m: m.group(1).lower()+m.group(2),sentence)
 
         # print(re.findall(r'([bcdfghjklmnpqrstvwxzčďǧɲʃťž])[|~#§ ](\1[mnɲ])', sentence))
-        sentence = re.sub(r'([bcdfghjklmnpqrstvwxzčďǧɲʃťž])[|~#§ ](\1[mnɲ])',r'\g<2>',sentence) # optional
+        sentence = re.sub(r'([bcdfghjklmnpqrstvwxzčďǧɲʃťž])[|~#§ ]?(\1[mnɲ])',r'\g<2>',sentence) # optional
 
         # print(re.findall(r'([BCDFGHJKLMNPQRSTVWXZČĎǦƝƩŤŽ])([mnɲ])', sentence))
         sentence = re.sub(r'([BCDFGHJKLMNPQRSTVWXZČĎǦƝƩŤŽ])([mnɲ])',lambda m: m.group(1).lower()+m.group(2),sentence)
@@ -339,14 +340,14 @@ class PhonemeConverter():
 
     
     def fast_m_nasalization(self, sentence: str):
-        sentence = re.sub(r'[mn][|#§~ ]?([pbfv])',r'ɱ\g<1>', sentence)
+        sentence = re.sub(r'[mn][|#§~ ]?([fv])',r'ɱ\g<1>', sentence) #pb
         return(self.fast_long_letters(sentence))
 
 
 
     def ipaization(self, sentence: str):
         ipa_sentence = ''
-        ipa = {'a':'ɒ', 'á':'aː', 'b':'b', 'c':'t͡s', 'č':'t͡ʃ', 'd':'d', 'ď':'d͡z', 'e':'ɛ', 'é':'eː', 'f':'f', 'g':'ɡ', 'ǧ':'ɟ', 'h':'h', 'i':'i', 'í':'iː', 'j':'j', 'k':'k', 'l':'l', 'ly':'j', 'm':'m', 'n':'n', 'ɲ':'ɲ', 'o':'o', 'ó':'oː', 'ö':'ø', 'ő':'øː', 'p':'p', 'q':'k', 'r':'r', 's':'ʃ', 'ʃ':'s', 't':'t', 'ť':'c', 'u':'u', 'ú':'uː', 'ü':'y', 'ű':'yː', 'v':'v', 'w':'v', 'x':'ks', 'y':'i', 'z':'z', 'ž':'ʒ', 'B':'bː', 'C':'t͡sː', 'Č':'t͡ʃː', 'D':'dː', 'Ď':'d͡zː', 'F':'fː', 'G':'ɡː', 'Ǧ':'ɟː', 'H':'hː', 'J':'jː', 'K':'kː', 'L':'lː', 'ly':'j', 'M':'mː', 'N':'nː', 'Ɲ':'ɲː', 'P':'pː', 'Q':'kː', 'R':'rː', 'S':'ʃː', 'Ʃ':'sː', 'T':'tː', 'Ť':'cː', 'V':'vː', 'W':'vː', 'X':'ksː', 'Z':'zː', 'Ž':'ʒː' }
+        ipa = {'a':'ɒ', 'á':'aː', 'b':'b', 'c':'t͡s', 'č':'t͡ʃ', 'd':'d', 'ď':'d͡z', 'e':'ɛ', 'é':'eː', 'f':'f', 'g':'ɡ', 'ǧ':'ɟ', 'h':'h', 'i':'i', 'í':'iː', 'j':'j', 'k':'k', 'l':'l', 'ly':'j', 'm':'m', 'n':'n', 'ɲ':'ɲ', 'o':'o', 'ó':'oː', 'ö':'ø', 'ő':'øː', 'p':'p', 'q':'k', 'r':'r', 's':'ʃ', 'ʃ':'s', 't':'t', 'ť':'c', 'u':'u', 'ú':'uː', 'ü':'y', 'ű':'yː', 'v':'v', 'w':'v', 'x':'ks', 'y':'i', 'z':'z', 'ž':'ʒ', 'B':'bː', 'C':'t͡sː', 'Č':'t͡ʃː', 'D':'dː', 'Ď':'d͡zː', 'F':'fː', 'G':'ɡː', 'Ǧ':'ɟː', 'H':'hː', 'J':'jː', 'K':'kː', 'L':'lː', 'ly':'j', 'M':'mː', 'N':'nː', 'Ɲ':'ɲː', 'P':'pː', 'Q':'kː', 'R':'rː', 'S':'ʃː', 'Ʃ':'sː', 'T':'tː', 'Ť':'cː', 'V':'vː', 'W':'vː', 'X':'ksː', 'Z':'zː', 'Ž':'ʒː', 'Ḧ':'xː', 'ḧ':'x' }
         #'dzs':'d͡ʒ',
         sentence = self.fast_long_letters(re.sub(r'[|~§#]', '', sentence))
         for letter in sentence:
